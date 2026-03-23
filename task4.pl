38a1ea0d5a34c7ee64ab48af35c5a57672432233
Saenize <artyomivshin20@gmail.com>
2026-03-23 18:21:21 +0500

    new

--- task4.pl
+++ task4.pl
@@ -1,18 +1,18 @@
 solve(A, N, V, G, D) :-
-        (A == true, D == true -> N == true ; true),
+        (((A == true), (D == true)) -> (N == true) ; true),
         (D == false -> (N == true, V == false) ; true),
-        (A \= V),
-        (D == true -> G == false ; true),
-        (N == false, V == false -> D == true ; true),
+        (A \== V),
+        (D == true -> (G == false) ; true),
+        (N == false, V == false -> D ; true),
         (V == true, N == false -> (D == false, G == true) ; true).
     
 	
 findSolutions :-
-    member(A, [true, fail]),
-    member(N, [true, fail]),
-    member(V, [true, fail]),
-    member(G, [true, fail]),
-    member(D, [true, fail]),
+    member(A, [true, false]),
+    member(N, [true, false]),
+    member(V, [true, false]),
+    member(G, [true, false]),
+    member(D, [true, false]),
     solve(A, N, V, G, D),
     write('Answer: '),
     (A == true -> write('A ') ; true),
